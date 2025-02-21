3c6c711f93d1cbbddb0e79ec55b2db14aed5a9f8
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-22 01:14:32 +0700

    update

--- iam.tf
+++ iam.tf
@@ -1,4 +1,12 @@
+# Check if the IAM role already exists
+data "aws_iam_role" "existing_role" {
+  name = "ecsInstanceRole"
+}
+
+# Create the IAM Role only if it doesn't exist
 resource "aws_iam_role" "ecs_instance_role" {
+  count = length(data.aws_iam_role.existing_role) > 0 ? 0 : 1
+
   name = "ecsInstanceRole"
 
   assume_role_policy = jsonencode({
@@ -13,12 +21,19 @@ resource "aws_iam_role" "ecs_instance_role" {
   })
 }
 
-resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecsInstanceProfile"
-  role = aws_iam_role.ecs_instance_role.name
+# Dynamically select the IAM Role Name
+locals {
+  iam_role_name = length(data.aws_iam_role.existing_role) > 0 ? data.aws_iam_role.existing_role.name : aws_iam_role.ecs_instance_role[0].name
 }
 
+# Create an instance profile for the role
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfile"
+  role = local.iam_role_name
+}
+
+# Attach the necessary policy to the IAM role
 resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
-  role       = aws_iam_role.ecs_instance_role.name
+  role       = local.iam_role_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 aws_region       = "ap-southeast-1"
-vpc_id           = "vpc-0abcd1234efgh5678"
+vpc_id           = "vpc-059c4f84bc0f46372"
 subnet_ids       = ["subnet-00d5bc668307b044a", "subnet-0562fae8bbdf8997a", "subnet-0cf3955b66b7b9417"]
 ecs_cluster_name = "primary-cluster"
 nlb              = "primary-nlb"
