2621a7c9e2b9d879acc8e359f7b4631a1940ac81
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-22 01:22:59 +0700

    update

--- asg.tf
+++ asg.tf
@@ -1,26 +1,27 @@
 
-resource "aws_launch_configuration" "ecs_launch_config" {
-  name            = "ecs-launch-config"
-  image_id        = "ami-0672fd5b9210aa093"
-  instance_type   = "t3.medium"
-  security_groups = [aws_security_group.ecs_sg.id]
+resource "aws_launch_template" "ecs_launch_template" {
+  name_prefix   = "ecs-launch-template"
+  image_id      = "ami-0672fd5b9210aa093"
+  instance_type = "t3.medium"
+  
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 25
+    }
+  }
 
-  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "ECS_CLUSTER=${var.ecs_cluster_name}" >> /etc/ecs/ecs.config
-              echo "ECS_ENABLE_CONTAINER_METADATA=true" >> /etc/ecs/ecs.config
-              yum install -y aws-cli
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ECS-Instance"
+    }
   }
 }
 
+
 resource "aws_autoscaling_group" "primary_ecs_asg" {
-  launch_configuration = aws_launch_configuration.ecs_launch_config.id
+  launch_configuration = aws_launch_template.ecs_launch_template.id
   vpc_zone_identifier  = var.subnet_ids
   min_size             = 2
   max_size             = 4
