56a3578becee20628c4636354f2545ea6eca75b9
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-22 09:00:27 +0700

    update

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 
 resource "aws_launch_template" "ecs_launch_template" {
-  name_prefix   = "ecs-launch-template"
+  name                = "primary-ecs-asg"
   image_id      = "ami-0672fd5b9210aa093"
   instance_type = "t3.medium"
   
@@ -14,7 +14,7 @@ resource "aws_launch_template" "ecs_launch_template" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "ECS-Instance"
+      Name = "primary-ecs-instance"
     }
   }
 }
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "primary_ecs_asg" {
 
   tag {
     key                 = "Name"
-    value               = "ECS-Instance"
+    value               = "primary-ecs-instance"
     propagate_at_launch = true
   }
 }
