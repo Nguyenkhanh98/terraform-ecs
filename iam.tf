b1d12337af4757aa0b880134a159709ae7168dcc
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-22 19:45:13 +0700

    update

--- iam.tf
+++ iam.tf
@@ -1,33 +1,25 @@
-
-# Create the IAM Role only if it doesn't exist
-data "aws_iam_role" "existing_ecs_instance_role" {
-  name = "ecsInstanceRole"
-}
-
 resource "aws_iam_role" "ecs_instance_role" {
-  count = length(data.aws_iam_role.existing_ecs_instance_role.arn) > 0 ? 0 : 1
-  name  = "ecsInstanceRole"
+  count = 1  # Ensure only one IAM role is created
+  name  = "ecs-instance-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
     Statement = [{
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
       }
-      Action = "sts:AssumeRole"
     }]
+    Version = "2012-10-17"
   })
 }
-# Create an instance profile for the role
+
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecsInstanceProfile"
-  role = aws_iam_role.ecs_instance_role.name
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role[0].name  # Fix: use [0] to reference the role
 }
 
-# Attach the necessary policy to the IAM role
 resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
-  role       = aws_iam_role.ecs_instance_role.name
+  role       = aws_iam_role.ecs_instance_role[0].name  # Fix: use [0] to reference the role
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
-
