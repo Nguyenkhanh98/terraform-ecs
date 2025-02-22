d80148be92e6e406410381743bbefe880a146881
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-22 08:50:17 +0700

    update

--- asg.tf
+++ asg.tf
@@ -21,9 +21,19 @@ resource "aws_launch_template" "ecs_launch_template" {
 
 
 resource "aws_autoscaling_group" "primary_ecs_asg" {
-  launch_configuration = aws_launch_template.ecs_launch_template.id
   vpc_zone_identifier  = var.subnet_ids
   min_size             = 2
   max_size             = 4
   desired_capacity     = 2
+
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ECS-Instance"
+    propagate_at_launch = true
+  }
 }
