273d49dca10ade8646f869aad9ae20bf3458f39a
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-24 11:59:32 +0700

    update var

--- variables.tf
+++ variables.tf
@@ -2,27 +2,33 @@
 
 
 variable "image_tag_cms" {
-   description = "The latest image digest for app1"
+   description = "The latest image tag for cms"
    type        = string
+   default    = "master-cms-latest"
 }
 variable "image_tag_fe" {
-   description = "The latest image digest for app1"
+   description = "The latest image tag for fe"
    type        = string
+   default    = "web-latest" 
+
 }
 
 variable "image_tag_host" {
-   description = "The latest image digest for app1"
+   description = "The latest image tag for host"
    type        = string
+    default    = "host-latest"
 }
 
 variable "image_tag_admin" {
-   description = "The latest image digest for app1"
+   description = "The latest image tag for admin"
    type        = string
+    default    = "admin-latest"
 }
 
 variable "image_tag_api" {
-   description = "The latest image digest for app1"
+   description = "The latest image tag for api"
    type        = string
+    default    = "main-api-latest"
 }
 
 
