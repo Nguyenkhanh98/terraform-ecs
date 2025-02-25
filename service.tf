7ae69b5ef0d4cea600ef30b4d97419b9d22ea08f
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-02-25 20:12:04 +0700

    update

--- service.tf
+++ service.tf
@@ -47,20 +47,20 @@ resource "aws_ecs_service" "onair_host_service" {
 }
 
 # ECS Service for API
-resource "aws_ecs_service" "onair_api_service" {
-  name                               = "onair_api-service"
-  cluster                            = aws_ecs_cluster.primary_cluster.id
-  task_definition                    = aws_ecs_task_definition.onair-api.arn
-  desired_count                      = 2
-  launch_type                        = "EC2"
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-  load_balancer {
-    target_group_arn = aws_lb_target_group.onair_api_tg.arn
-    container_name   = "onair-api-container"
-    container_port   = 3005
-  }
-}
+# resource "aws_ecs_service" "onair_api_service" {
+#   name                               = "onair_api-service"
+#   cluster                            = aws_ecs_cluster.primary_cluster.id
+#   task_definition                    = aws_ecs_task_definition.onair-api.arn
+#   desired_count                      = 2
+#   launch_type                        = "EC2"
+#   deployment_minimum_healthy_percent = 50
+#   deployment_maximum_percent         = 200
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.onair_api_tg.arn
+#     container_name   = "onair-api-container"
+#     container_port   = 3005
+#   }
+# }
 
 # ECS Service for Strapi CMS
 resource "aws_ecs_service" "cms_service" {
