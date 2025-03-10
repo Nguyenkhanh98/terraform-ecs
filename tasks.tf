7bbca5389ffe359ba6a968814ee55a1598fad816
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-03-10 14:33:11 +0700

    update

--- tasks.tf
+++ tasks.tf
@@ -123,8 +123,8 @@ resource "aws_ecs_task_definition" "onair-api" {
   family                   = "onair-api-task"
   requires_compatibilities = ["EC2"]
   network_mode             = "bridge"
-  memory                   = "512"
-  cpu                      = "256"
+  memory                   = "1024"
+  cpu                      = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
