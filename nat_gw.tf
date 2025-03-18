4bd2790ce7005b806e573604ee2bf9a756036b87
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-03-18 23:02:29 +0700

    re-construct network

--- nat_gw.tf
+++ nat_gw.tf
@@ -1,10 +1,8 @@
 resource "aws_eip" "nat" {
-  count = length(var.subnet_ids)
   domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "nat" {
-  count = length(var.subnet_ids)
 
   allocation_id = aws_eip.nat.id
   subnet_id     = var.subnet_ids[0]
@@ -25,4 +23,10 @@ resource "aws_route" "nat_gateway_route" {
   route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private_rt.id
 }
