183206c4bd716fbbce76310be2debd84cf9f29ba
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-03-18 22:02:52 +0700

    re-construct network

--- nat_gw.tf
+++ nat_gw.tf
@@ -21,7 +21,7 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route" "nat_gateway_route" {
   count = length(var.subnet_ids)
 
-  route_table_id         = aws_route_table.private_rt[count.index].id
+  route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
