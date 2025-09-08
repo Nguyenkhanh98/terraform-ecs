978c3796480b780b53abfc18b50486967a90e116
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-09-09 04:46:36 +0700

    update

--- ldecs/alb_rule.tf
+++ ldecs/alb_rule.tf
@@ -1,35 +1,31 @@
-# resource "aws_lb_listener_rule" "onair_ld_https_rule" {
-#   listener_arn = aws_lb_listener.onair_ld_https_listener.arn
-#   priority     = 30
+resource "aws_lb_listener_rule" "onair_ld_https_rule" {
+  listener_arn = aws_lb_listener.onair_ld_https_listener.arn
+  priority     = 30
 
-#   condition {
-#     host_header {
-#       values = ["ldvietnam.com"]
-#     }
-#   }
+  condition {
+    host_header {
+      values = ["ldvietnam.com"]
+    }
+  }
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.onair_ld_tg.arn
-#   }
-# }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.onair_ld_tg.arn
+  }
+}
 
-# resource "aws_lb_listener" "onair_ld_https_listener" {
-#   load_balancer_arn = aws_lb.onair_ld_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = "arn:aws:acm:us-east-1:497082176439:certificate/f40bfb38-7228-4877-adf3-87ac9c75c9c4"
+resource "aws_lb_listener" "onair_ld_https_listener" {
+  load_balancer_arn = aws_lb.onair_ld_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:ap-southeast-1:497082176439:certificate/705ed308-0dea-45cf-9637-655272411fb6"
 
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "Not Found"
-#       status_code  = "404"
-#     }
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.onair_ld_tg.arn
+  }
+}
 # resource "aws_lb_listener" "onair_http_redirect" {
 #   load_balancer_arn = aws_lb.onair_ld_alb.arn
 #   port              = 80
