dd517f3cd41334d1d876a572f2848efc11a0ac64
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-09-09 04:49:43 +0700

    update

--- ldecs/alb_rule.tf
+++ ldecs/alb_rule.tf
@@ -26,31 +26,31 @@ resource "aws_lb_listener" "onair_ld_https_listener" {
     target_group_arn = aws_lb_target_group.onair_ld_tg.arn
   }
 }
-# resource "aws_lb_listener" "onair_http_redirect" {
-#   load_balancer_arn = aws_lb.onair_ld_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type = "redirect"
-#     redirect {
-#       protocol    = "HTTPS"
-#       port        = "443"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
-
-resource "aws_lb_listener" "onair_http_listener" {
+resource "aws_lb_listener" "onair_http_redirect" {
   load_balancer_arn = aws_lb.onair_ld_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.onair_ld_tg.arn
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
   }
 }
+
+# resource "aws_lb_listener" "onair_http_listener" {
+#   load_balancer_arn = aws_lb.onair_ld_alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.onair_ld_tg.arn
+#   }
+# }
 
 
 resource "aws_lb_target_group" "onair_ld_tg" {
@@ -71,7 +71,7 @@ resource "aws_lb_target_group" "onair_ld_tg" {
 
 
 resource "aws_lb_listener_rule" "onair_ld_http_rule" {
-  listener_arn = aws_lb_listener.onair_http_listener.arn
+  listener_arn = aws_lb_listener.onair_http_redirect.arn
   priority     = 30
 
   condition {
